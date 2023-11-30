a59152896e2faffad00f038cd7338d0cf87b1c29
WilliamGC05 <wgc4@usca.edu>
2023-11-30 12:05:18 -0500

    Database

---
+++ js/SignIn.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD6KA0ECIAB3scXNUu7mxgjmbmnhjOSEVc",
+    authDomain: "banksia-pages-database.firebaseapp.com",
+    projectId: "banksia-pages-database",
+    storageBucket: "banksia-pages-database.appspot.com",
+    messagingSenderId: "239610874789",
+    appId: "1:239610874789:web:80bcede53d7ef4ba26764c",
+    measurementId: "G-JV3QXR7P2H"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  
