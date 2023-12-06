346de591c0c73a168420af657a172e98492805c3
WilliamGC05 <wgc4@usca.edu>
2023-12-06 11:57:44 -0500

    Google Login

--- js/SignIn.js
+++ js/SignIn.js
@@ -14,4 +14,34 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 
+  //Google login
+  $('#google-logo').click(function () {
+    var provider = new firebase.auth.GoogleAuthProvider();
   
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+  
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      
+      // User is signed in.
+      console.log("Sign in through Google username: " + user);
+  
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      console.log("Error: " + errorMessage);
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  });
