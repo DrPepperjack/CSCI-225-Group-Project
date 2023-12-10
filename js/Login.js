cc141e82596433d6bfc3f2fccaf4b2708115f021
WilliamGC05 <wgc4@usca.edu>
2023-12-10 18:31:45 -0500

    Testing Authentication

--- js/Login.js
+++ js/Login.js
@@ -1,18 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyD6KA0ECIAB3scXNUu7mxgjmbmnhjOSEVc",
-    authDomain: "banksia-pages-database.firebaseapp.com",
-    projectId: "banksia-pages-database",
-    storageBucket: "banksia-pages-database.appspot.com",
-    messagingSenderId: "239610874789",
-    appId: "1:239610874789:web:80bcede53d7ef4ba26764c",
-    measurementId: "G-JV3QXR7P2H"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 // add  a google login choice here 
 $('#google-login-button').click(function () {
     var provider = new firebase.auth.GoogleAuthProvider();
--- js/auth-listener.js
+++ js/auth-listener.js
@@ -1,17 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyD6KA0ECIAB3scXNUu7mxgjmbmnhjOSEVc",
-  authDomain: "banksia-pages-database.firebaseapp.com",
-  projectId: "banksia-pages-database",
-  storageBucket: "banksia-pages-database.appspot.com",
-  messagingSenderId: "239610874789",
-  appId: "1:239610874789:web:80bcede53d7ef4ba26764c",
-  measurementId: "G-JV3QXR7P2H"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-
 const navbar = document.getElementById('navbar');
 
 firebase.auth().onAuthStateChanged((user)=>{ //listener for auth state change
